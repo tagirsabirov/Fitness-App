12a0372aad3bc62567ab162b89d0f28d6c77227f
Tagir Sabirov <tagirsabirov1@gmail.com>
2021-07-12 17:37:19 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	  name='FitnessGame',
+	  version='0.1',
+	  description='An OpenCV based application',
+	  url='https://github.com/tagirsabirov/Innocamp-Hackathon-2',
+	  author='Tagir Sabirov and Timur Vakhrushev',
+	  packages=['opencv-python', 'mediapipe'],
+	  )
